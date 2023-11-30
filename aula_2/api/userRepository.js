238ecbab52b16b63a8257706dc6e48cab4fe181a
Camila Moura Rodrigues de Miranda <cmiranda@fastprobr.com>
2023-11-30 01:06:55 -0300

    feat: pod

---
+++ aula_2/api/userRepository.js
@@ -0,0 +1,17 @@
+class UserRepository {
+  save(nome, email, anoNascimento) {
+    const anoAtual = new Date().getFullYear();
+
+    let idade = anoAtual - anoNascimento;
+
+    let resultado = {
+      nome: nome,
+      email: email,
+      idade: idade,
+    };
+
+    return resultado;
+  }
+}
+
+module.exports = UserRepository;
